788da53fc88fb7d8c7d0df5444003fdf42671202
AyushVerma2004 <vermaayush20180122139@gmail.com>
2026-02-17 16:53:33 +0530

    Added explore Menu

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import './Header.css'
 
 const header = () => {
   return (
     <div className='header'>
+      <div className="headercontents">
+        <h2>Order Food Here !</h2>
+        <p>Choose From a diverse menu featuring a delectable array of dishes crafted with the finest ingridients and Culinary expertise. Our main goal is to statisfy your craving and elevate your dining experience, one delicious meal at a time.</p>
+        <button>View Menu</button>
+      </div>
       
     </div>
   )
--- Frontend/src/pages/home/home.jsx
+++ Frontend/src/pages/home/home.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './home.css'
+import Header from '../../Components/Header/Header'
+import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
 
 const home = () => {
+
+  const [category,setCategory] = useState("All");
+
   return (
     <div>
-      <header/>
+      <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }
